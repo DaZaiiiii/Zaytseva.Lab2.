c1e8a7076158a5141d19fd135d38f5153e28f7bf
DaZaiiiii <90222734+DaZaiiiii@users.noreply.github.com>
2021-09-29 02:00:28 +0300

    Add files via upload

---
+++ Zaytseva.Lab2.Exersise1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Zaytseva.Lab2.Exersise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Даны три стороны треугольника, найти периметр и площадь.
+            Console.WriteLine("Введите стороны треугольника");
+            int a, b, c, P, S, p;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+
+            // Периметр.
+            P = a + b + c;
+            Console.WriteLine("периметр: " + P);
+
+            // Полупериметр.
+            p = (1 / 2) * (a + b + c);
+            Console.WriteLine("полупериметр: " + p);
+
+            // Площадь.
+            int v = p * (p - a);
+            int d = (p - b);
+            int f = (p - c);
+
+            S = Math.Sqrt(​v * d * f);
+            Console.WriteLine("площадь: " + S);
+
+        }
+    }
+}
