daf308931be9be379096123076678a09ee0d55e7
DaZaiiiii <90222734+DaZaiiiii@users.noreply.github.com>
2021-10-12 13:55:11 +0300

    Update Program.cs

--- Zaytseva.Lab2.Exersise2/Program.cs
+++ Zaytseva.Lab2.Exersise2/Program.cs
@@ -10,7 +10,7 @@ namespace Zaytseva.Lab2.Exersise2
             {
                 throw new ArgumentNullException(nameof(args));
             }
-            // f(x) = 1/43 + ln(|3x/4|) + x/7 + min(x,-3) + cos(x/3)/sin(x/3) + x^x
+            // f(x) = 1/43,0 + ln(|3x/4|) + x/7 + min(x,-3) + cos(x/3)/sin(x/3) + x^x
 
             // Выводим информацию на консоль.
             Console.WriteLine("Введите x:");
@@ -23,7 +23,7 @@ namespace Zaytseva.Lab2.Exersise2
 
             // Объявляем y.
             double y;
-            y = 1 / 43 + Math.Log(Math.Abs(x / 3)) + x / 7 + Math.Min(x, -3) + Math.Cos(x / 3) / Math.Sin(x / 3) + Math.Pow(x, x);
+            y = 1 / 43,0 + Math.Log(Math.Abs(x / 3)) + x / 7 + Math.Min(x, -3) + Math.Cos(x / 3) / Math.Sin(x / 3) + Math.Pow(x, x);
 
             // Выводим информацию о значение y.
             Console.WriteLine(y);
